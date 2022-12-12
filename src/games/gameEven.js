bad33b8c412d69221830d66e3a73c5da08f2c547
seenerve <seenerve@gmail.com>
2022-12-12 13:02:58 +0600

    fix even

--- src/games/gameEven.js
+++ src/games/gameEven.js
@@ -9,7 +9,7 @@ const isEven = (number) => number % 2 === 0;
 
 const evenLogic = () => {
   const question = getRandomNumber(minNumber, maxNumber);
-  const answer = isEven(maxNumber) ? 'yes' : 'no';
+  const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
 const startEvenGame = () => {
