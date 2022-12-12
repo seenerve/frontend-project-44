ea9f0c1c5cb39b783de29bf06ea86116535f82d3
seenerve <seenerve@gmail.com>
2022-12-12 12:14:52 +0600

    fix maxNumbers

--- src/games/gameEven.js
+++ src/games/gameEven.js
@@ -6,7 +6,7 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 const isEven = (number) => number % 2 === 0;
 
 const evenLogic = () => {
-  const maxNumber = getRandomNumber(1, 100);
+  const maxNumber = getRandomNumber(1, 50);
   const answer = isEven(maxNumber) ? 'yes' : 'no';
   return [maxNumber, answer];
 };
